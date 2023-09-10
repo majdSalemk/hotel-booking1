bdf70c100a7366458c3dce60c21e1250658fe989
ghaidaataleb <ghaidaataleb94@gmail.com>
2023-09-10 17:35:51 +0300

    update

--- app/Http/Controllers/Front/BookingController.php
+++ app/Http/Controllers/Front/BookingController.php
@@ -2,22 +2,22 @@
 
 namespace App\Http\Controllers\Front;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Customer;
-use App\Models\Order;
-use App\Models\OrderDetail;
-use App\Models\BookedRoom;
+use Stripe;
 use App\Models\Room;
-use Auth;
-use DB;
-use App\Mail\Websitemail;
+use App\Models\Order;
 use PayPal\Api\Amount;
 use PayPal\Api\Details;
 use PayPal\Api\Payment;
-use PayPal\Api\PaymentExecution;
+use App\Models\Customer;
+use App\Mail\Websitemail;
+use App\Models\BookedRoom;
+use App\Models\OrderDetail;
 use PayPal\Api\Transaction;
-use Stripe;
+use Illuminate\Http\Request;
+use PayPal\Api\PaymentExecution;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class BookingController extends Controller
 {
@@ -182,172 +182,172 @@ class BookingController extends Controller
         return view('front.payment');
     }
 
-    public function paypal($final_price)
-    {
-        $client = 'ARw2VtkTvo3aT7DILgPWeSUPjMK_AS5RlMKkUmB78O8rFCJcfX6jFSmTDpgdV3bOFLG2WE-s11AcCGTD';
-        $secret = 'EPi7BbZ0b5GP9jmy095MyNkfYjJc3PF42fC58emf-FXRZF7kEUmHKpV0rfGl6EEWXUx0TSvo0FmXkzuy';
+    // public function paypal($final_price)
+    // {
+        // $client = 'ARw2VtkTvo3aT7DILgPWeSUPjMK_AS5RlMKkUmB78O8rFCJcfX6jFSmTDpgdV3bOFLG2WE-s11AcCGTD';
+        // $secret = 'EPi7BbZ0b5GP9jmy095MyNkfYjJc3PF42fC58emf-FXRZF7kEUmHKpV0rfGl6EEWXUx0TSvo0FmXkzuy';
 
-        $apiContext = new \PayPal\Rest\ApiContext(
-            new \PayPal\Auth\OAuthTokenCredential(
-                $client, // ClientID
-                $secret // ClientSecret
-            )
-        );
+        // $apiContext = new \PayPal\Rest\ApiContext(
+        //     new \PayPal\Auth\OAuthTokenCredential(
+        //         $client, // ClientID
+        //         $secret // ClientSecret
+        //     )
+        // );
 
-        $paymentId = request('paymentId');
-        $payment = Payment::get($paymentId, $apiContext);
+        // $paymentId = request('paymentId');
+        // $payment = Payment::get($paymentId, $apiContext);
 
-        $execution = new PaymentExecution();
-        $execution->setPayerId(request('PayerID'));
+        // $execution = new PaymentExecution();
+        // $execution->setPayerId(request('PayerID'));
 
-        $transaction = new Transaction();
-        $amount = new Amount();
-        $details = new Details();
+        // $transaction = new Transaction();
+        // $amount = new Amount();
+        // $details = new Details();
 
-        $details->setShipping(0)
-            ->setTax(0)
-            ->setSubtotal($final_price);
+        // $details->setShipping(0)
+        //     ->setTax(0)
+        //     ->setSubtotal($final_price);
 
-        $amount->setCurrency('USD');
-        $amount->setTotal($final_price);
-        $amount->setDetails($details);
-        $transaction->setAmount($amount);
-        $execution->addTransaction($transaction);
-        $result = $payment->execute($execution, $apiContext);
+        // $amount->setCurrency('USD');
+        // $amount->setTotal($final_price);
+        // $amount->setDetails($details);
+        // $transaction->setAmount($amount);
+        // $execution->addTransaction($transaction);
+    //     $result = true;
 
-        if ($result->state == 'approved') {
-            $paid_amount = $result->transactions[0]->amount->total;
+    //     if ($result) {
+    //         $paid_amount = $result;
 
-            $order_no = time();
+    //         $order_no = time();
 
-            $statement = DB::select("SHOW TABLE STATUS LIKE 'orders'");
-            $ai_id = $statement[0]->Auto_increment;
+    //         $statement = DB::select("SHOW TABLE STATUS LIKE 'orders'");
+    //         $ai_id = $statement[0]->Auto_increment;
 
-            $obj = new Order();
-            $obj->customer_id = Auth::guard('customer')->user()->id;
-            $obj->order_no = $order_no;
-            $obj->transaction_id = $result->id;
-            $obj->payment_method = 'PayPal';
-            $obj->paid_amount = $paid_amount;
-            $obj->booking_date = date('d/m/Y');
-            $obj->status = 'Completed';
-            $obj->save();
+    //         $obj = new Order();
+    //         $obj->customer_id = Auth::guard('customer')->user()->id;
+    //         $obj->order_no = $order_no;
+    //         $obj->transaction_id = $result->id;
+    //         $obj->payment_method = 'PayPal';
+    //         $obj->paid_amount = $paid_amount;
+    //         $obj->booking_date = date('d/m/Y');
+    //         $obj->status = 'Completed';
+    //         $obj->save();
 
-            $arr_reservation_room_id = array();
-            $i = 0;
-            foreach (session()->get('reservation_room_id') as $value) {
-                $arr_reservation_room_id[$i] = $value;
-                $i++;
-            }
+    //         $arr_reservation_room_id = array();
+    //         $i = 0;
+    //         foreach (session()->get('reservation_room_id') as $value) {
+    //             $arr_reservation_room_id[$i] = $value;
+    //             $i++;
+    //         }
 
-            $arr_reservation_checkin_date = array();
-            $i = 0;
-            foreach (session()->get('reservation_checkin_date') as $value) {
-                $arr_reservation_checkin_date[$i] = $value;
-                $i++;
-            }
+    //         $arr_reservation_checkin_date = array();
+    //         $i = 0;
+    //         foreach (session()->get('reservation_checkin_date') as $value) {
+    //             $arr_reservation_checkin_date[$i] = $value;
+    //             $i++;
+    //         }
 
-            $arr_reservation_checkout_date = array();
-            $i = 0;
-            foreach (session()->get('reservation_checkout_date') as $value) {
-                $arr_reservation_checkout_date[$i] = $value;
-                $i++;
-            }
+    //         $arr_reservation_checkout_date = array();
+    //         $i = 0;
+    //         foreach (session()->get('reservation_checkout_date') as $value) {
+    //             $arr_reservation_checkout_date[$i] = $value;
+    //             $i++;
+    //         }
 
-            $arr_reservation_adult = array();
-            $i = 0;
-            foreach (session()->get('reservation_adult') as $value) {
-                $arr_reservation_adult[$i] = $value;
-                $i++;
-            }
+    //         $arr_reservation_adult = array();
+    //         $i = 0;
+    //         foreach (session()->get('reservation_adult') as $value) {
+    //             $arr_reservation_adult[$i] = $value;
+    //             $i++;
+    //         }
 
-            $arr_reservation_children = array();
-            $i = 0;
-            foreach (session()->get('reservation_children') as $value) {
-                $arr_reservation_children[$i] = $value;
-                $i++;
-            }
+    //         $arr_reservation_children = array();
+    //         $i = 0;
+    //         foreach (session()->get('reservation_children') as $value) {
+    //             $arr_reservation_children[$i] = $value;
+    //             $i++;
+    //         }
 
-            for ($i = 0; $i < count($arr_reservation_room_id); $i++) {
-                $r_info = Room::where('id', $arr_reservation_room_id[$i])->first();
-                $d1 = explode('/', $arr_reservation_checkin_date[$i]);
-                $d2 = explode('/', $arr_reservation_checkout_date[$i]);
-                $d1_new = $d1[2] . '-' . $d1[1] . '-' . $d1[0];
-                $d2_new = $d2[2] . '-' . $d2[1] . '-' . $d2[0];
-                $t1 = strtotime($d1_new);
-                $t2 = strtotime($d2_new);
-                $diff = ($t2 - $t1) / 60 / 60 / 24;
-                $sub = $r_info->price * $diff;
+    //         for ($i = 0; $i < count($arr_reservation_room_id); $i++) {
+    //             $r_info = Room::where('id', $arr_reservation_room_id[$i])->first();
+    //             $d1 = explode('/', $arr_reservation_checkin_date[$i]);
+    //             $d2 = explode('/', $arr_reservation_checkout_date[$i]);
+    //             $d1_new = $d1[2] . '-' . $d1[1] . '-' . $d1[0];
+    //             $d2_new = $d2[2] . '-' . $d2[1] . '-' . $d2[0];
+    //             $t1 = strtotime($d1_new);
+    //             $t2 = strtotime($d2_new);
+    //             $diff = ($t2 - $t1) / 60 / 60 / 24;
+    //             $sub = $r_info->price * $diff;
 
-                $obj = new OrderDetail();
-                $obj->order_id = $ai_id;
-                $obj->room_id = $arr_reservation_room_id[$i];
-                $obj->order_no = $order_no;
-                $obj->checkin_date = $arr_reservation_checkin_date[$i];
-                $obj->checkout_date = $arr_reservation_checkout_date[$i];
-                $obj->adult = $arr_reservation_adult[$i];
-                $obj->children = $arr_reservation_children[$i];
-                $obj->subtotal = $sub;
-                $obj->save();
+    //             $obj = new OrderDetail();
+    //             $obj->order_id = $ai_id;
+    //             $obj->room_id = $arr_reservation_room_id[$i];
+    //             $obj->order_no = $order_no;
+    //             $obj->checkin_date = $arr_reservation_checkin_date[$i];
+    //             $obj->checkout_date = $arr_reservation_checkout_date[$i];
+    //             $obj->adult = $arr_reservation_adult[$i];
+    //             $obj->children = $arr_reservation_children[$i];
+    //             $obj->subtotal = $sub;
+    //             $obj->save();
 
-                while (1) {
-                    if ($t1 >= $t2) {
-                        break;
-                    }
+    //             while (1) {
+    //                 if ($t1 >= $t2) {
+    //                     break;
+    //                 }
 
-                    $obj = new BookedRoom();
-                    $obj->booking_date = date('d/m/Y', $t1);
-                    $obj->order_no = $order_no;
-                    $obj->room_id = $arr_reservation_room_id[$i];
-                    $obj->save();
+    //                 $obj = new BookedRoom();
+    //                 $obj->booking_date = date('d/m/Y', $t1);
+    //                 $obj->order_no = $order_no;
+    //                 $obj->room_id = $arr_reservation_room_id[$i];
+    //                 $obj->save();
 
-                    $t1 = strtotime('+1 day', $t1);
-                }
-            }
+    //                 $t1 = strtotime('+1 day', $t1);
+    //             }
+    //         }
 
-            $subject = 'New Order';
-            $message = 'You have made an order for hotel booking. The booking information is given below: <br>';
-            $message .= '<br>Order No: ' . $order_no;
-            $message .= '<br>Transaction Id: ' . $result->id;
-            $message .= '<br>Payment Method: PayPal';
-            $message .= '<br>Paid Amount: ' . $paid_amount;
-            $message .= '<br>Booking Date: ' . date('d/m/Y') . '<br>';
+    //         $subject = 'New Order';
+    //         $message = 'You have made an order for hotel booking. The booking information is given below: <br>';
+    //         $message .= '<br>Order No: ' . $order_no;
+    //         $message .= '<br>Transaction Id: ' . $result->id;
+    //         $message .= '<br>Payment Method: PayPal';
+    //         $message .= '<br>Paid Amount: ' . $paid_amount;
+    //         $message .= '<br>Booking Date: ' . date('d/m/Y') . '<br>';
 
-            for ($i = 0; $i < count($arr_reservation_room_id); $i++) {
+    //         for ($i = 0; $i < count($arr_reservation_room_id); $i++) {
 
-                $r_info = Room::where('id', $arr_reservation_room_id[$i])->first();
+    //             $r_info = Room::where('id', $arr_reservation_room_id[$i])->first();
 
-                $message .= '<br>Room Name: ' . $r_info->name;
-                $message .= '<br>Price Per Night: $' . $r_info->price;
-                $message .= '<br>Checkin Date: ' . $arr_reservation_checkin_date[$i];
-                $message .= '<br>Checkout Date: ' . $arr_reservation_checkout_date[$i];
-                $message .= '<br>Adult: ' . $arr_reservation_adult[$i];
-                $message .= '<br>Children: ' . $arr_reservation_children[$i] . '<br>';
-            }
+    //             $message .= '<br>Room Name: ' . $r_info->name;
+    //             $message .= '<br>Price Per Night: $' . $r_info->price;
+    //             $message .= '<br>Checkin Date: ' . $arr_reservation_checkin_date[$i];
+    //             $message .= '<br>Checkout Date: ' . $arr_reservation_checkout_date[$i];
+    //             $message .= '<br>Adult: ' . $arr_reservation_adult[$i];
+    //             $message .= '<br>Children: ' . $arr_reservation_children[$i] . '<br>';
+    //         }
 
-            $customer_email = Auth::guard('customer')->user()->email;
+    //         $customer_email = Auth::guard('customer')->user()->email;
 
-            \Mail::to($customer_email)->send(new Websitemail($subject, $message));
+    //         \Mail::to($customer_email)->send(new Websitemail($subject, $message));
 
-            session()->forget('reservation_room_id');
-            session()->forget('reservation_checkin_date');
-            session()->forget('reservation_checkout_date');
-            session()->forget('reservation_adult');
-            session()->forget('reservation_children');
-            session()->forget('billing_name');
-            session()->forget('billing_email');
-            session()->forget('billing_phone');
-            session()->forget('billing_country');
-            session()->forget('billing_address');
-            session()->forget('billing_state');
-            session()->forget('billing_city');
-            session()->forget('billing_zip');
+    //         session()->forget('reservation_room_id');
+    //         session()->forget('reservation_checkin_date');
+    //         session()->forget('reservation_checkout_date');
+    //         session()->forget('reservation_adult');
+    //         session()->forget('reservation_children');
+    //         session()->forget('billing_name');
+    //         session()->forget('billing_email');
+    //         session()->forget('billing_phone');
+    //         session()->forget('billing_country');
+    //         session()->forget('billing_address');
+    //         session()->forget('billing_state');
+    //         session()->forget('billing_city');
+    //         session()->forget('billing_zip');
 
-            return redirect()->route('home')->with('success', 'Payment is successful');
-        } else {
-            return redirect()->route('home')->with('error', 'Payment is failed');
-        }
-    }
+    //         return redirect()->route('home')->with('success', 'Payment is successful');
+    //     } else {
+    //         return redirect()->route('home')->with('error', 'Payment is failed');
+    //     }
+    // }
 
     public function stripe(Request $request, $final_price)
     {
@@ -490,6 +490,17 @@ class BookingController extends Controller
         session()->forget('billing_state');
         session()->forget('billing_city');
         session()->forget('billing_zip');
+
+        return redirect()->route('home')->with('success', 'Payment is successful');
+
+    }
+
+    public function processPayment(Request $request)
+    {
+        // Validate the payment method and any other relevant data
+        $request->validate([
+            'payment_method' => 'required', // Add your validation rules for payment_method
+        ]);
 
         return redirect()->route('home')->with('success', 'Payment is successful');
     }
